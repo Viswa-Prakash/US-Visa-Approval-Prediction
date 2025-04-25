34262c871eb9e70628c993fa5c49321521e23be1
“Viswa <prakashviswa1990@gmail.com>
2025-04-25 14:10:21 +0530

    requirements and setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us-visa-approval-prediction",
+    version="0.0.1",
+    description="Predicting US Visa Approvals",
+    author="Viswa Prakash",
+    author_email="prakashviswa1990@gmail.com",
+    packages=find_packages()
+)
