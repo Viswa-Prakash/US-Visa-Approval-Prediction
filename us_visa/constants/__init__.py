5fc2e4bb2fa6c9791308aa86384a20d14c9e9b33
“Viswa <prakashviswa1990@gmail.com>
2025-04-29 01:09:33 +0530

    Prediction Pipeline added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -63,5 +63,10 @@ REGION_NAME = "us-east-1"
 
 
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "usvisabucket2024"
+MODEL_BUCKET_NAME = "usvisas3"
 MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
